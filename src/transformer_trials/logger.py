79a867ab8a8616d0c5c795fbf65df648c3791773
Micha Albert <micha@2231puppy.tech>
2025-05-19 00:34:43 -0400

    working on logging

--- src/transformer_trials/logger.py
+++ src/transformer_trials/logger.py
@@ -1,33 +1,42 @@
 import logging
 
+from colorama import Fore, Back, Style
+
 
 class CustomFormatter(logging.Formatter):
 
-    blue = "\x1b[34;20m"
-    green = "\x1b[32;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format_str = (
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-    )
+    format_str = f"%(asctime)s - {Style.BRIGHT}%(levelname)s{Style.NORMAL} from %(name)s: %(message)s {Style.BRIGHT}(%(filename)s:%(lineno)d){Style.NORMAL}"
 
     FORMATS = {
-        logging.DEBUG: blue + format_str + reset,
-        logging.INFO: green + format_str + reset,
-        logging.WARNING: yellow + format_str + reset,
-        logging.ERROR: red + format_str + reset,
-        logging.CRITICAL: bold_red + format_str + reset,
+        logging.DEBUG: Fore.BLUE + format_str + Style.RESET_ALL,
+        logging.INFO: Fore.GREEN + format_str + Style.RESET_ALL,
+        logging.WARNING: Fore.YELLOW + format_str + Style.RESET_ALL,
+        logging.ERROR: Fore.RED + format_str + Style.RESET_ALL,
+        logging.CRITICAL: Style.BRIGHT + Fore.RED + format_str + Style.RESET_ALL,
     }
 
     def format(self, record: logging.LogRecord):
+        # print(record)
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
 
-def get_logger(level: int = logging.INFO):
+class LogHijack(logging.Filter):
+    def filter(self, record: logging.LogRecord):
+        print(record)
+        logging.getLogger(__name__).handle(record)
+        return False
+
+
+def get_logger(level: int = logging.DEBUG):
+    for log in (
+        logging.getLogger("uvicorn"),
+        logging.getLogger("uvicorn.error"),
+        logging.getLogger("uvicorn.access"),
+        logging.getLogger("uvicorn.asgi"),
+    ):
+        log.addFilter(LogHijack())
     logger = logging.getLogger(__name__)
     logger.setLevel(level)
 
--- src/transformer_trials/main.py
+++ src/transformer_trials/main.py
@@ -1,20 +1,43 @@
+import asyncio
 import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+import click
+import hypercorn
+import uvloop
 from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from hypercorn.asyncio import serve as hypercorn_serve
+from hypercorn.config import Config
 from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 from slack_bolt.async_app import AsyncApp
 
-from .logger import get_logger
+from .logger import LogHijack, get_logger
 
 logger = get_logger(logging.DEBUG)
 
-load_dotenv()
+config: Config
+
+class CustomConfig(Config):
+    def __init__(self):
+        self.logger_class = logging.Logger
+        super().__init__()
+
+@click.group()
+def cli():
+    global config
+    config = CustomConfig()
+    load_dotenv()
+    logger.info("Starting server...")
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    yield
+
 
 app = AsyncApp()
 app_handler = AsyncSlackRequestHandler(app)
-
 
 @app.event("app_mention")
 async def handle_app_mentions(body, say):
@@ -26,14 +49,33 @@ async def handle_app_mentions(body, say):
 async def handle_message():
     pass
 
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    yield
-
-
 api = FastAPI(lifespan=lifespan)
 
 @api.post("/slack/events")
 async def endpoint(req: Request):
     return await app_handler.handle(req)
+
+
+@cli.command()
+@click.option(
+    "--reload", "-r", is_flag=True, help="If specified, server will auto reload."
+)
+@click.option(
+    "--port",
+    "-p",
+    default=8000,
+    help="Port on which to serve the API. Defaults to 8000.",
+)
+@click.option(
+    "--host",
+    "-i",
+    default="127.0.0.1",
+    help="IP address on which to bind the server. Defaults to 127.0.0.1.",
+)
+def serve(reload: bool, port: int, host: str):
+    # uvloop.install()
+    asyncio.run(hypercorn_serve(app=api, config=config)) # type: ignore
+
+
+if __name__ == "__main__":
+    cli()
